d246f885c5ba143562ea161c864c4a6efd4a474a
Pratibha _2 <px00825638@techmahindra.com>
2023-01-11 13:27:21 +0530

    book review application added

---
+++ final_project/router/general.js
@@ -0,0 +1,70 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Write your code here
+  return res.send(JSON.stringify({books},null,4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  const isbn=req.params.isbn;
+ 
+  return res.send(books[isbn]);
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  //Write your code here
+  const author=req.params.author;
+
+  for(let i=1;i<=10;i++){
+      if(books[i].author===author)
+      return res.send(books[i])
+
+  }
+
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  const title=req.params.title;
+
+  for(let i=1;i<=10;i++){
+      if(books[i].title===title)
+      return res.send(books[i])
+
+  }
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+ const isbn=req.params.isbn;
+ return res.send(books[isbn].reviews)
+});
+
+module.exports.general = public_users;
